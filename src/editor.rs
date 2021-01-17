8a78163b96c3dcf46e9672d7046785cf976d1148
Unnikrishnan Nair, Rahul <rahul.unnikrishnan.nair@intel.com>
2021-01-17 12:59:36 -0800

    added navigation

--- src/editor.rs
+++ src/editor.rs
@@ -9,9 +9,14 @@ fn die(e: std::io::Error) {
     panic!(e);
 }
 
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
+    cursor_position: Position,
 }
 
 impl Editor {
@@ -34,17 +39,19 @@ impl Editor {
         Self {
             should_quit: false,
             terminal: Terminal::default().unwrap(),
+            cursor_position: Position { x: 0, y: 0 },
         }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::hide_cursor();
+        Terminal::cursor_position(&Position { x: 0, y: 0 });
         if self.should_quit {
             Terminal::clear_screen();
             println!("Good bye!\r");
         } else {
             self.draw_rows();
-            Terminal::cursor_position(0, 0);
+            Terminal::cursor_position(&Position { x: 0, y: 0 });
         }
         Terminal::show_cursor();
         Terminal::flush()
@@ -54,20 +61,71 @@ impl Editor {
         // wait for keypress to read key
         // read_key is wrapped in a Result
         // either a valid key or an Error
-        let processed_key = Terminal::read_key()?;
-        match processed_key {
+        let pressed_key = Terminal::read_key()?;
+        match pressed_key {
             // if key is ctrl + q, end program
             Key::Ctrl('q') => self.should_quit = true,
+            Key::Up
+            | Key::Down
+            | Key::Left
+            | Key::Right
+            | Key::PageUp
+            | Key::PageDown
+            | Key::End
+            | Key::Home => self.move_cursor(pressed_key),
             _ => (),
         }
         // if everything goes well without error
         // everything is Ok, return nothing - ()
         Ok(())
     }
+
+    fn move_cursor(&mut self, key: Key) {
+        let Position { mut y, mut x } = self.cursor_position;
+        let size = self.terminal.size();
+        let height = size.height.saturating_sub(1) as usize;
+        let width = size.width.saturating_sub(1) as usize;
+        match key {
+            Key::Up => y = y.saturating_sub(1),
+            Key::Down => {
+                if y < height {
+                    y = y.saturating_add(1);
+                }
+            }
+            Key::Right => {
+                if x < width {
+                    x = x.saturating_add(1);
+                }
+            }
+            Key::PageUp => y = 0,
+            Key::PageDown => y = height,
+            Key::Home => x = 0,
+            Key::End => x = width,
+            Key::Left => x = x.saturating_sub(1),
+            _ => (),
+        }
+        self.cursor_position = Position { x, y }
+    }
+
+    fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("Adam editor -- version {}\r", VERSION);
+        let terminal_width = self.terminal.size().width as usize;
+        let message_len = welcome_message.len();
+        let padding = terminal_width.saturating_sub(message_len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(terminal_width);
+        println!("{}\r", welcome_message);
+    }
     fn draw_rows(&self) {
-        for _ in 0..self.terminal.size().height - 1 {
+        let height = self.terminal.size().height;
+        for row in 0..height - 1 {
             Terminal::clear_current_line();
-            println!("~\r");
+            if row == height / 3 {
+                self.draw_welcome_message();
+            } else {
+                println!("~\r");
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ mod editor;
 mod terminal;
 
 use editor::Editor;
+pub use editor::Position;
 pub use terminal::Terminal;
+
 fn main() {
     Editor::default().run()
 }
